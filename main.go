aa3aceac26d70f9fc724c49f3c56a1bc6c445304
ilya <ilya.issakin@gmail.com>
2022-06-03 19:15:35 +0600

    Export excel file to browser

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/xuri/excelize/v2"
 	"io"
 	"log"
 	"net/http"
@@ -23,9 +24,6 @@ func convert(rec []string) (Lap, error) {
 	if err != nil {
 		return Lap{}, err
 	}
-	//if laptime > LAPTIME_THRESHOLD {
-	//	return Lap{}, types.Error{}
-	//}
 	num, _ := strconv.Atoi(rec[0])
 	var class string
 	if len(rec[16]) > 1 {
@@ -48,6 +46,13 @@ func formatTime(s string) string {
 	formatted := strings.ReplaceAll(s, ":", "m")
 	formatted = strings.ReplaceAll(formatted, ".", "s")
 	return formatted + "ms"
+}
+
+type Timespan time.Duration
+
+func (t Timespan) Format(format string) string {
+	z := time.Unix(0, 0).UTC()
+	return z.Add(time.Duration(t)).Format(format)
 }
 
 func handleUpload(w http.ResponseWriter, r *http.Request) {
@@ -89,7 +94,77 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 	}
 
 	sheetsData := processLaps(laps)
-	fmt.Println(sheetsData)
+
+	excelFile := createProtocol(sheetsData)
+	defer excelFile.Close()
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Content-Disposition", "attachment; filename=protocol.xlsx")
+	w.Header().Set("Content-Transfer-Encoding", "binary")
+	excelFile.Write(w)
+}
+
+func createProtocol(data []TAClass) *excelize.File {
+	protocol := excelize.NewFile()
+	//timeFormatStyle, _ := protocol.NewStyle(&excelize.Style{NumFmt: 47})
+	//bestLapStyle, _ := protocol.NewStyle(&excelize.Style{Fill: excelize.Fill{
+	//	Pattern: 1,
+	//	Color:   []string{"yellow"},
+	//	Shading: 0,
+	//}})
+	var initialY = 'A'
+	var initialX = 2
+
+	for _, taClass := range data {
+		protocol.NewSheet(taClass.Name)
+		// header row
+		protocol.SetCellValue(taClass.Name, "B1", "Пилот")
+		protocol.SetCellValue(taClass.Name, "C1", "No")
+		protocol.SetCellValue(taClass.Name, "D1", "1 Круг")
+		protocol.SetCellValue(taClass.Name, "E1", "2 Круг")
+		protocol.SetCellValue(taClass.Name, "F1", "3 Круг")
+		protocol.SetCellValue(taClass.Name, "G1", "1 Сессия")
+		protocol.SetCellValue(taClass.Name, "H1", "1 Круг")
+		protocol.SetCellValue(taClass.Name, "I1", "2 Круг")
+		protocol.SetCellValue(taClass.Name, "J1", "3 Круг")
+		protocol.SetCellValue(taClass.Name, "K1", "2 Сессия")
+		protocol.SetCellValue(taClass.Name, "L1", "1 Круг")
+		protocol.SetCellValue(taClass.Name, "M1", "2 Круг")
+		protocol.SetCellValue(taClass.Name, "N1", "3 Круг")
+		protocol.SetCellValue(taClass.Name, "O1", "3 Сессия")
+		protocol.SetCellValue(taClass.Name, "P1", "Итог")
+
+		for di, driver := range taClass.Drivers {
+			//protocol.SetRowStyle(taClass.Name, di, di, timeFormatStyle)
+			i := 0
+			protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), di+1)
+			i++
+			protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), driver.DriverName)
+			i++
+			protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), driver.DriverNo)
+			i++
+
+			for _, session := range driver.Sessions {
+				for _, lapTime := range session.LapTimes {
+					protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), Timespan(lapTime).Format("04:05.000"))
+					i++
+				}
+				for l := len(session.LapTimes); l < LAPS_IN_SESSION; l++ {
+					protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), "–")
+					i++
+				}
+				protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), Timespan(session.BestLap).Format("04:05.000"))
+				//protocol.SetCellStyle(taClass.Name, string(initialY+int32(i)), strconv.Itoa(initialX+di), bestLapStyle)
+				i++
+			}
+
+			protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), Timespan(driver.TotalTime).Format("04:05.000"))
+			i++
+		}
+	}
+
+	protocol.DeleteSheet("Sheet1")
+	return protocol
 }
 
 func processLaps(laps []Lap) []TAClass {
