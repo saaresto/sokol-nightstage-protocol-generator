c2ac9c7e79c575752b503061b644ccac59231baf
ilya <ilya.issakin@gmail.com>
2022-06-12 11:49:51 +0600

    Add styling to protocol

--- protocol_handler.go
+++ protocol_handler.go
@@ -61,12 +61,106 @@ func handleProtocolUpload(w http.ResponseWriter, r *http.Request) {
 
 func createProtocol(data []TAClass) *excelize.File {
 	protocol := excelize.NewFile()
-	//timeFormatStyle, _ := protocol.NewStyle(&excelize.Style{NumFmt: 47})
-	//bestLapStyle, _ := protocol.NewStyle(&excelize.Style{Fill: excelize.Fill{
-	//	Pattern: 1,
-	//	Color:   []string{"yellow"},
-	//	Shading: 0,
-	//}})
+	sessionBestStyle, _ := protocol.NewStyle(&excelize.Style{
+		Fill: excelize.Fill{
+			Type:    "pattern",
+			Pattern: 1,
+			Color:   []string{"3cb03a"},
+		},
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+		},
+		Font: &excelize.Font{
+			Size:   16,
+			Bold:   true,
+			Italic: true,
+		},
+	})
+	headerStyle, _ := protocol.NewStyle(&excelize.Style{
+		Fill: excelize.Fill{
+			Type:    "pattern",
+			Pattern: 1,
+			Color:   []string{"1c399e"},
+		},
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+		},
+		Font: &excelize.Font{
+			Size:  14,
+			Color: "ffffff",
+			Bold:  true,
+		},
+	})
+	redStyle, _ := protocol.NewStyle(&excelize.Style{
+		Fill: excelize.Fill{
+			Type:    "pattern",
+			Pattern: 1,
+			Color:   []string{"f71e1e"},
+		},
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+		},
+		Font: &excelize.Font{
+			Size: 14,
+			Bold: true,
+		},
+	})
+	greyStyle, _ := protocol.NewStyle(&excelize.Style{
+		Fill: excelize.Fill{
+			Type:    "pattern",
+			Pattern: 1,
+			Color:   []string{"999999"},
+		},
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+		},
+		Font: &excelize.Font{
+			Size: 14,
+			Bold: true,
+		},
+	})
+	whiteStyle, _ := protocol.NewStyle(&excelize.Style{
+		Fill: excelize.Fill{
+			Type:    "pattern",
+			Pattern: 1,
+			Color:   []string{"ffffff"},
+		},
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+		},
+		Font: &excelize.Font{
+			Size: 14,
+		},
+	})
+	bestLapStyle, _ := protocol.NewStyle(&excelize.Style{
+		Fill: excelize.Fill{
+			Type:    "pattern",
+			Pattern: 1,
+			Color:   []string{"8b13c2"},
+		},
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+		},
+		Font: &excelize.Font{
+			Size:  14,
+			Color: "ffffff",
+		},
+	})
+	totalStyle, _ := protocol.NewStyle(&excelize.Style{
+		Fill: excelize.Fill{
+			Type:    "pattern",
+			Pattern: 1,
+			Color:   []string{"f59236"},
+		},
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+		},
+		Font: &excelize.Font{
+			Size:   16,
+			Bold:   true,
+			Italic: true,
+		},
+	})
 	var initialY = 'A'
 	var initialX = 2
 
@@ -88,38 +182,50 @@ func createProtocol(data []TAClass) *excelize.File {
 		protocol.SetCellValue(taClass.Name, "N1", "3 Круг")
 		protocol.SetCellValue(taClass.Name, "O1", "3 Сессия")
 		protocol.SetCellValue(taClass.Name, "P1", "Итог")
+		protocol.SetCellStyle(taClass.Name, "B1", "P1", headerStyle)
 
 		for di, driver := range taClass.Drivers {
 			//protocol.SetRowStyle(taClass.Name, di, di, timeFormatStyle)
 			i := 0
-			protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), di+1)
+			addStyledCell(protocol, taClass, initialY, i, initialX, di, di+1, redStyle)
 			i++
-			protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), driver.DriverName)
+			addStyledCell(protocol, taClass, initialY, i, initialX, di, driver.DriverName, greyStyle)
 			i++
-			protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), driver.DriverNo)
+			addStyledCell(protocol, taClass, initialY, i, initialX, di, driver.DriverNo, greyStyle)
 			i++
 
 			for _, session := range driver.Sessions {
 				for _, lapTime := range session.LapTimes {
-					protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), Timespan(lapTime).Format("04:05.000"))
+					if lapTime == session.BestLap {
+						addStyledCell(protocol, taClass, initialY, i, initialX, di, Timespan(lapTime).Format("04:05.000"), bestLapStyle)
+					} else {
+						addStyledCell(protocol, taClass, initialY, i, initialX, di, Timespan(lapTime).Format("04:05.000"), whiteStyle)
+					}
 					i++
 				}
 				for l := len(session.LapTimes); l < LAPS_IN_SESSION; l++ {
-					protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), "–")
+					addStyledCell(protocol, taClass, initialY, i, initialX, di, "–", greyStyle)
 					i++
 				}
-				protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), Timespan(session.BestLap).Format("04:05.000"))
-				//protocol.SetCellStyle(taClass.Name, string(initialY+int32(i)), strconv.Itoa(initialX+di), bestLapStyle)
+				addStyledCell(protocol, taClass, initialY, i, initialX, di, Timespan(session.BestLap).Format("04:05.000"), sessionBestStyle)
 				i++
 			}
 
-			protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), Timespan(driver.TotalTime).Format("04:05.000"))
+			addStyledCell(protocol, taClass, initialY, i, initialX, di, Timespan(driver.TotalTime).Format("04:05.000"), totalStyle)
 			i++
 		}
 	}
 
 	protocol.DeleteSheet("Sheet1")
 	return protocol
+}
+
+func addStyledCell(protocol *excelize.File, taClass TAClass, initialY int32, i int, initialX int, di int, value interface{}, style int) {
+	protocol.SetCellValue(taClass.Name, string(initialY+int32(i))+strconv.Itoa(initialX+di), value)
+	protocol.SetCellStyle(taClass.Name,
+		string(initialY+int32(i))+strconv.Itoa(initialX+di),
+		string(initialY+int32(i))+strconv.Itoa(initialX+di),
+		style)
 }
 
 func processLaps(laps []Lap) []TAClass {
